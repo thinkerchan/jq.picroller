e4ec7689d798a65ad1cc179bd55c530629f03073
Thinker Chan <291810213@qq.com>
2016-08-30 20:50:31 +0800

    update
    update

--- README.md
+++ README.md
@@ -1,6 +1,28 @@
 # jquery.rollingslider.js
-jq3d图片轮播插件 兼容IE6+
-欢迎提需求或者提bug
 
-##demo地址
+## 仿3D图片轮播
+### HTML结构
+	<div class="demo">
+  		<ul class="slide-wrap" id="JslideWrap">
+    		<li class="pos1"></li>
+		    <li class="pos2"></li>
+		    <li class="pos3"></li>
+		    <li class="pos4"></li>
+		    <li class="pos5"></li>  
+		</ul>
+  		<i class="arrow prev" id="Jprev"></i>
+		<i class="arrow next" id="Jnext"></i>
+	</div>
+### JS用法
+	var demo = new PicRoller({
+				showArea:"#JslideWrap",	//必须参数
+				prev:"#Jprev",	//必须参数
+				next:"#Jnext",	//必须参数
+				column:5,	//展示列数
+				midClass:"active",	//中间li的样式名
+				moveSpeed:300,	//动画时间
+				autoPlay:false,	//自动播放
+				stay:3000	//autoPlay为true时使用才有效果
+			});
+## demo地址
 [http://theutopia.cn/demo/jquery.rollingslider.js/](http://theutopia.cn/demo/jquery.rollingslider.js/)
--- script/jquery.rollingslider.js
+++ script/jquery.rollingslider.js
@@ -1,174 +1,146 @@
-(function($,window,document,undefined) {
-  var argFn = function(ele,obj){
-    this.$element = ele,
-    this.defaults = {
-        showArea:"#JslideWrap",
-        prev:"#Jprev",
-        next:"#Jnext",
-        moveSpeed: 400,
-        autoPlay:false,
-        stay: 5000
-    },
-    this.args = $.extend({}, this.defaults, obj);
+/**
+ * @thinkerchan 291810213@qq.com
+ */
+;(function($, window, undefined) {
+  var defaults = {
+    showArea:"#JslideWrap",
+    prev:"#Jprev",
+    next:"#Jnext",
+    midClass:"active",
+    moveSpeed:400,
+    autoPlay:false,
+    stay:5e3,
+    column:5
   };
-  $.fn.RollingSlider = function(arg) {
-    var _instance = new argFn(this, arg);
-    // 参数, 函数声明
-    var $showArea = $(_instance.args.showArea),
-        $showArea_li = $(_instance.args.showArea+">li"),
-        $prev =  $(_instance.args.prev),
-        $next =  $(_instance.args.next),
-        show_len = $showArea_li.length,
-        column = 5,
-        direction = "next",
-        timer,
-        init_arr = [],
-        item_arr = [],  // 存起所有的li
-        flag = 1;
-    // 存储样式和li
-    function slider() {
-      for (var i = 0; i < show_len; i++) {
-        var $cur_li = $showArea_li.eq(i); //当前展示的图片
-        if (i < column) { //记录 5 张图片的初始状态信息
-          init_arr[i] = {
-            left: $cur_li.position().left,
-            top: $cur_li.position().top,
-            zIndex: $cur_li.css("z-index"),
-            width: $cur_li.width(),
-            height: $cur_li.height()
-          };
-          $cur_li.css("left", init_arr[i].left)
-        } else {
-          $cur_li.css("left", init_arr[column - 1].left)
-        }
-        item_arr.push($cur_li);
-      }
-    }
-    //模拟点击和设置鼠标离开指定区域而恢复自动轮播
-    function simulate() {
-      $next.click();
-    }
-    function autoPlay() {
-      timer = setInterval(simulate, _instance.args.stay);
-    }
-    function clearTimer(ele){
-      $(ele).bind("mouseenter",
-        function() {
-          clearInterval(timer)
-        }).bind("mouseleave",
-        function() {
-          if(_instance.args.autoPlay===true){
-            autoPlay();
-          }
-        });
-    }
-    // 箭头
-    function arrows() {
-      clearTimer(_instance.args.showArea);
-      clearTimer(_instance.args.prev);
-      clearTimer(_instance.args.next);
-      $next.bind("click",function() {
-        if (flag) {
-          direction = "next";
-          flag = 0;
-          animation();
-        }
-      });
-      $prev.bind("click",function() {
-        if (flag) {
-          direction = "prev";
-          flag = 0;
-          animation();
-        }
-      });
-    }
-    //动画
-    function animation() {
-      if (direction == "next") { //点击next
-        for (i = 0; i < column; i++) {
-          var prevStyleObj = init_arr[i - 1];
-          if (i == 0) {
-            item_arr[i].fadeOut(_instance.args.moveSpeed)
-          } else {
-            item_arr[i].css("z-index", prevStyleObj.zIndex).animate({
-              left: prevStyleObj.left,
-              top: prevStyleObj.top,
-              width: prevStyleObj.width,
-              height: prevStyleObj.height
-            }, _instance.args.moveSpeed)
-          }
-        }
-        var lastStyleObj = init_arr[column - 1]; // 最后一个样式对象
-        if (item_arr.length != column) { //只能大于
-          item_arr[column].css({
-            left: lastStyleObj.left,
-            top: lastStyleObj.top,
-            width: lastStyleObj.width,
-            height:lastStyleObj.height,
-            "z-index": lastStyleObj.zIndex
-          }).fadeIn(_instance.args.moveSpeed,function() {
-            flag = 1;
-          })
-        } else {  // 或者等于
-          item_arr[0].stop().css({
-            left: lastStyleObj.left,
-            top: lastStyleObj.top,
-            width: lastStyleObj.width,
-            height:lastStyleObj.height,
-            "z-index": lastStyleObj.zIndex
-          }).fadeIn(_instance.args.moveSpeed,function() {
-            flag = 1;
-          })
-        }
-        item_arr.push(item_arr.shift());
-        // 按需加载右边图片
-        lazyLoad(4);
-      } else {    //点击prev
-        for (i = 0; i < column; i++) {
-          var nextStyleObj = init_arr[i + 1];
-          if (i == column - 1) {
-            item_arr[i].css("z-index", 0).fadeOut(_instance.args.moveSpeed)
-          } else {
-            item_arr[i].css("z-index", nextStyleObj.zIndex).animate({
-              left: nextStyleObj.left,
-              top: nextStyleObj.top,
-              width: nextStyleObj.width,
-              height:nextStyleObj.height
-            },_instance.args.moveSpeed)
-          }
-        }
-        var firstStyleObj = init_arr[0];
-        item_arr[item_arr.length - 1].stop().css({
-          left: firstStyleObj.left,
-          top: firstStyleObj.top,
-          width: firstStyleObj.width,
-          height:firstStyleObj.height,
-          "z-index": firstStyleObj.zIndex
-        }).fadeIn(_instance.args.moveSpeed,function() {
-          flag = 1;
-        });
-        item_arr.unshift(item_arr.pop());
-        //按需加载左边图片
-        lazyLoad(0);
-      }
-    }
-    // 按需加载
-    function lazyLoad(index){
-      var $nextOne = item_arr[index].find('img');
-      var $realSrc = $nextOne.data('src');
-      var hasSrc = item_arr[index].find('img').attr('src');
-      if (!hasSrc) {
-        $nextOne.attr('src', $realSrc);
-      };
-    }
-    // 初始化
-    function init() {
-      slider();
-      arrows();
-      if(_instance.args.autoPlay===true){
-        autoPlay();
-      }
-    }
-    init();
+  // 区域全局变量
+  var $showArea, $showArea_li, $prev, $next, _this, show_len, column, flag, midClass, moveSpeed, timer, styleArr = [], itemArr = [];
+  // 构造函数
+  function PicRoller(options) {
+    $.extend(this, defaults, options);
+    _this = this;
+    this.init();
   }
-})($,window,document);
+  PicRoller.prototype = {
+    init:function() {
+      $showArea = $(this.showArea);
+      $showArea_li = $(this.showArea + ">li");
+      $prev = $(this.prev);
+      $next = $(this.next);
+      show_len = $showArea_li.length;
+      column = this.column;
+      midClass = this.midClass;
+      flag = 1;
+      moveSpeed = this.moveSpeed;
+      if (show_len < column) {
+        alert("数量设置错误!");
+        return false;
+      }
+      this.saveStyle();
+      this.arrows();
+      this.autoPlay && this._autoPlay();
+    },
+    // 存储所有的show-item 但只存储 column个 样式
+    saveStyle:function() {
+      for (var i = 0; i < show_len; i++) {
+        var $cur_li = $showArea_li.eq(i);
+        if (i < column) {
+          // 存储用户设置的样式
+          styleArr[i] = {
+            left:$cur_li.position().left,   // offset vs position
+            top:$cur_li.position().top,
+            zIndex:$cur_li.css("z-index"),
+            width:$cur_li.width(),
+            height:$cur_li.height()
+          };
+          $cur_li.css("left", styleArr[i].left);
+        } else {
+          // 超出部分取最后一个样式
+          $cur_li.css("left", styleArr[column - 1].left);
+        }
+        // 存储所有的show-item;
+        itemArr.push($cur_li);
+      }
+    },
+    arrows:function() {
+      this._prev();
+      this._next();
+    },
+    _prev:function() {
+      this.bindClick($prev, "prev");
+    },
+    _next:function() {
+      this.bindClick($next, "next");
+    },
+    bindClick:function(prevOrNext, direction) {
+      prevOrNext.bind("click", function() {
+        if (flag) {
+          flag = 0;
+          _this.animation(direction);
+        }
+      });
+    },
+    animation:function(direction) {
+      var updateFlag = function(){
+        flag = 1;
+      };
+      if (direction == "next") {
+        for (i = 0; i < column; i++) {
+          var prevStyleObj = styleArr[i - 1]; // 获取前一个li的样式
+          if (i == 0) {
+            // 处理最左边
+            itemArr[i].fadeOut(moveSpeed);
+          } else {
+            itemArr[i].css({"z-index": prevStyleObj.zIndex}).animate(prevStyleObj, moveSpeed);
+          }
+        }
+        var lastStyleObj = styleArr[column - 1]; // 最右边的li样式
+        if (itemArr.length != column) {
+          itemArr[column].css(lastStyleObj).fadeIn(moveSpeed,updateFlag);
+        } else {
+          itemArr[0].stop().css(lastStyleObj).fadeIn(moveSpeed,updateFlag);
+        }
+        itemArr.push(itemArr.shift());
+        this.lazyLoad(column - 1);
+      } else {
+        for (i = 0; i < column; i++) {
+          var nextStyleObj = styleArr[i + 1]; //获取后一个li的样式
+          if (i == column - 1) {
+            // 处理最右边
+            itemArr[i].css("z-index", 0).fadeOut(moveSpeed);
+          } else {
+            itemArr[i].css("z-index", nextStyleObj.zIndex).animate(nextStyleObj, moveSpeed);
+          }
+        }
+        var firstStyleObj = styleArr[0];
+        itemArr[itemArr.length - 1].stop().css(firstStyleObj).fadeIn(moveSpeed,updateFlag);
+        itemArr.unshift(itemArr.pop());
+        this.lazyLoad(0);
+      }
+      var center  =  parseInt((column-1)/2);
+      itemArr[center].addClass(midClass).siblings().removeClass(midClass);
+    },
+    lazyLoad:function(index) {
+      var $nextOne = itemArr[index].find("img"),
+          $realSrc = $nextOne.data("src"),
+          hasSrc = itemArr[index].find("img").attr("src");
+      !hasSrc && $nextOne.attr("src", $realSrc);
+    },
+    simulate:function() {
+      $next.click();
+    },
+    _autoPlay:function(){
+      timer = setInterval(_this.simulate, _this.stay);
+      this.clearTimer();
+    },
+    clearTimer:function() {
+      $showArea.one("mouseenter", function() {
+        clearInterval(timer);
+        timer = null;
+      })
+      $showArea.one("mouseleave", function() {
+        _this.autoPlay && _this._autoPlay();
+      });
+    }
+  };
+  window.PicRoller = PicRoller;
+})($, window);
